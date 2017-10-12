4ef2d392a910d67e86fc642af69422a6108c3b1b
hafidz <hafidzzpermana09@gmail.com>
2017-10-12 08:07:47 +0700

    add action button to timeline

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ $(".tweet-button").click(function() {
 	var str1 = "<article class='media' id='first'><figure class='media-left'><p class='image is-64x64'><img src='images/myselfie.jpeg' style='border-radius: 50%'></p></figure><div class='media-content'><div class='content'><p><strong>Hafidz Permana</strong><small>@hafidzzpermana</small> <small>31m</small><br>";
 	var str2 = "</p></div><nav class='level is-mobile'><div class='level-left'><a class='level-item'><span class='icon is-small'><i class='fa fa-reply'></i></span></a><a class='level-item'><span class='icon is-small'><i class='fa fa-retweet'></i></span></a><a class='level-item'><span class='icon is-small'><i class='fa fa-heart'></i></span></a></div</nav></div><div class='media-right'><button class='delete btn-delete-first'></button></div></article>"
 	$("")
-	$(".empty").append(str1 + value + str2);
+	$(".empty").append(str1 + value + str2).after();
 });
 
 $(".btn-delete-first").click(function() {
